6b479e15c82d4264e4b4e3afae8a944819d5012c
Ricardo Lamooglia <riklamoglia@gmail.com>
2023-12-13 02:05:38 -0300

    Co-authored-by: Marcelo Bracet  <marcelo.bracet1@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
-function toggleMode(){
- const html = document.documentElement
- 
-   html.classList.toggle("light")
+const nome = document.getElementById("nome")
+const descricao = document.getElementById("descricao")
 
-   const img = document.querySelector("#profile img")
+// variáveis
+const projetos = document.getElementById("projetos")
+const curriculo = document.getElementById("curriculo")
+const codigo_fonte = document.getElementById("codigo_fonte")
+const certificados = document.getElementById("certificados")
 
+function toggleMode() {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
 
- if (html.classList.contains("light")) {
-   img.setAttribute("src", "./assets/Foto-dia.png.png")
- } else {
-   img.setAttribute("src", "./assets/Foto-noite.jpeg")
- }
+  html.classList.toggle("light")
 
-  
+  if (html.classList.contains("light")) {
+    // Changes to light mode
+    img.setAttribute("src", "./assets/Foto-dia.png.png")
+    nome.innerText = "Ricardo Gonçalves Lamoglia"
+
+    descricao.innerText =
+      "Estudante de Engenharia da Computação, estudei um ano e meio no INATEL vendo C++, atualmente segundo período na Estadio de Sá "
+
+    projetos.innerText = "Meu portifólio"
+    projetos.setAttribute(
+      "href",
+      "https://github.com/RicardoLamoglia?tab=repositories"
+    )
+
+    curriculo.innerText = "Meu currículo"
+    curriculo.setAttribute(
+      "href",
+      "https://drive.google.com/file/d/1ifGkAFZgqrkA2wb-lYRSeIgucEEKekMa/view"
+    )
+
+    codigo_fonte.innerText = "Código fonte desse projeto"
+    codigo_fonte.setAttribute(
+      "href",
+      "https://github.com/RicardoLamoglia/Projeto1"
+    )
+
+    certificados.innerText = "Meus Certificados"
+    certificados.setAttribute("href")
+
+    // Adicione aqui outras modificações necessárias para o modo claro
+  } else {
+    // Changes to dark mode
+    img.setAttribute("src", "./assets/Foto-noite.jpeg")
+    nome.innerText = "Lamoglia"
+    descricao.innerText = "Conheça mais sobre mim"
+
+    projetos.innerText = "Um pouco de mim"
+    projetos.setAttribute(
+      "href",
+      "https://drive.google.com/file/d/17Fkxy_WQ9Yrv06neXTb2PDkwXaH75Aml/view?usp=sharing"
+    )
+
+    curriculo.innerText = "Localização"
+    curriculo.setAttribute(
+      "href",
+      "https://www.google.com.br/maps/place/R.+Amilcar+de+Castro,+133+-+Jacarepaguá,+Rio+de+Janeiro+-+RJ,+22775-053/@-22.9696762,-43.3882035,17z/data=!3m1!4b1!4m6!3m5!1s0x9bdbdf8bff203b:0x2e23ffea664a5e39!8m2!3d-22.9696812!4d-43.3856232!16s%2Fg%2F11g8bcg114?entry=ttu"
+    )
+
+    codigo_fonte.innerText = "Número pessoal"
+    codigo_fonte.setAttribute(
+      "href",
+      "https://api.whatsapp.com/send/?phone=21983836713&text&type=phone_number&app_absent=0"
+    )
+
+    certificados.innerText = "Meus Certificados"
+    certificados.setAttribute("href")
+
+    // Adicione aqui outras modificações necessárias para o modo escuro
+  }
 }
